7a6475053735cbfee0d29b4b83ef45f1d0dba326
ahmadajlouni3 <bootsetup1@gmail.com>
2026-08-08 14:58:09 +0300

    Solution 6

--- main.js
+++ main.js
@@ -1,5 +1,54 @@
-console.log('A')
-setTimeout(() => console.log('B'), 0)
-Promise.resolve().then(() => console.log('C'))
-queueMicrotask(() => console.log('D'))
-console.log('E')
+const readline = require("readline");
+const rl = readline.createInterface({ input: process.stdin });
+const lines = [];
+
+class EvenRange {
+    constructor(start, end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    [Symbol.iterator]() {
+        let current = this.start;
+        const end = this.end;
+
+        return {
+            next() {
+                // تخطَّ الأعداد الفردية
+                while (current < end && current % 2 !== 0) {
+                    current++;
+                }
+
+                // انتهى النطاق
+                if (current >= end) {
+                    return {
+                        value: undefined,
+                        done: true
+                    };
+                }
+
+                // خزّن العدد الزوجي الحالي
+                const value = current;
+
+                // انتقل إلى الرقم التالي
+                current++;
+
+                return {
+                    value,
+                    done: false
+                };
+            }
+        };
+    }
+}
+
+rl.on("line", (line) => {
+    lines.push(line);
+    if (lines.length === 2) {
+        const start = parseInt(lines[0]);
+        const end = parseInt(lines[1]);
+        for (const n of new EvenRange(start, end)) console.log(n);
+        rl.close();
+    }
+});
+rl.on("close", () => process.exit(0));
