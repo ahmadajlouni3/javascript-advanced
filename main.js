8e59802ac6a51ec144a17ec7005040d89f9304fa
ahmadajlouni3 <bootsetup1@gmail.com>
2026-08-08 15:02:08 +0300

    Solution 8

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 const readline = require("readline");
 const rl = readline.createInterface({ input: process.stdin });
+const lines = [];
 
-function factorial(n) {
-    let result = 1n;
-
-    for (let i = 2; i <= n; i++) {
-        result *= BigInt(i);
-    }
-
-    return result;
+function Vec3(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
 }
 
+
 rl.on("line", (line) => {
-    const n = parseInt(line);
-    console.log(factorial(n).toString());
-    rl.close();
+    lines.push(line);
+    if (lines.length === 5) {
+        const vecs = lines.map(line => {
+            const [x, y, z] = line.split(' ').map(Number);
+            return new Vec3(x, y, z);
+        });
+        const total = vecs.reduce((s, v) => s + v.x, 0);
+        console.log(total);
+        rl.close();
+    }
 });
 rl.on("close", () => process.exit(0));
