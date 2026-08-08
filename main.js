5bfcbeaab1b9f3a9180857f28fec0944cb34c97e
ahmadajlouni3 <bootsetup1@gmail.com>
2026-08-08 15:00:09 +0300

    Solution 7

--- main.js
+++ main.js
@@ -1,54 +1,19 @@
 const readline = require("readline");
 const rl = readline.createInterface({ input: process.stdin });
-const lines = [];
 
-class EvenRange {
-    constructor(start, end) {
-        this.start = start;
-        this.end = end;
+function factorial(n) {
+    let result = 1n;
+
+    for (let i = 2; i <= n; i++) {
+        result *= BigInt(i);
     }
 
-    [Symbol.iterator]() {
-        let current = this.start;
-        const end = this.end;
-
-        return {
-            next() {
-                // تخطَّ الأعداد الفردية
-                while (current < end && current % 2 !== 0) {
-                    current++;
-                }
-
-                // انتهى النطاق
-                if (current >= end) {
-                    return {
-                        value: undefined,
-                        done: true
-                    };
-                }
-
-                // خزّن العدد الزوجي الحالي
-                const value = current;
-
-                // انتقل إلى الرقم التالي
-                current++;
-
-                return {
-                    value,
-                    done: false
-                };
-            }
-        };
-    }
+    return result;
 }
 
 rl.on("line", (line) => {
-    lines.push(line);
-    if (lines.length === 2) {
-        const start = parseInt(lines[0]);
-        const end = parseInt(lines[1]);
-        for (const n of new EvenRange(start, end)) console.log(n);
-        rl.close();
-    }
+    const n = parseInt(line);
+    console.log(factorial(n).toString());
+    rl.close();
 });
 rl.on("close", () => process.exit(0));
